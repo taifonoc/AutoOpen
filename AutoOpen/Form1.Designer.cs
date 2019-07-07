2badde96d2d1d42eff753fee83039940a2011999
tom30913 tom30913 <tom90440@gmail.com>
2019-07-07 20:49:09 +0800

    加入專案檔案。

---
+++ AutoOpen/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AutoOpen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "圖書館"});
+            this.comboBox1.Location = new System.Drawing.Point(104, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "專案類型";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "M";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(417, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "頻寬%數";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(104, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(413, 224);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "T100 (中正區)",
+            "T103 (大同區)",
+            "T104 (中山區)",
+            "T105 (松山區)",
+            "T106 (大安區)",
+            "T108 (萬華區)",
+            "T110 (信義區)",
+            "T111 (士林區)",
+            "T112 (北投區)",
+            "T114 (內湖區)",
+            "T115 (南港區)",
+            "T116 (文山區)"});
+            this.comboBox2.Location = new System.Drawing.Point(104, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "行政區";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(279, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ AutoOpen/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoOpen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string ww = comboBox1.SelectedItem.ToString();
+            switch (ww)
+            {
+                case "圖書館":
+                    richTextBox1.Text = $@" dba-profile m35_10% create
+mode sr
+sla maximum 39424
+apply
+!
+ extended-vlan-tagging-operation 605-1 create
+  downstream-mode enable
+  untagged-frame 1
+   treat outer vid 605 cos 0 tpid 0x8100
+   treat inner vid 1 cos 0 tpid 0x8100
+  apply
+ !
+ traffic-profile S605_0n4b_35 create
+  mgmt-mode uni eth 1 omci
+  mgmt-mode uni eth 2 omci
+  mgmt-mode uni eth 3 omci
+  mgmt-mode uni eth 4 omci
+  tcont 1
+   gemport 1/1
+   dba-profile m35_10%
+  tcont 2
+   gemport 2/1
+   dba-profile 512k
+  mapper 1
+   gemport count 1
+   gemport 1 rate-limit upstream 39424 39424
+   gemport 1 rate-limit downstream 39424 39424
+  mapper 2
+   gemport count 1
+   gemport 1 rate-limit upstream 512 512
+   gemport 1 rate-limit downstream 512 512
+  bridge 1
+   ani mapper 1
+   uni eth 1
+    extended-vlan-tagging-operation 605-1
+    rate-limit upstream 39424 39424
+    rate-limit downstream 39424 39424
+   uni eth 2
+    extended-vlan-tagging-operation 605-1
+    rate-limit upstream 39424 39424
+    rate-limit downstream 39424 39424
+   uni eth 3
+    extended-vlan-tagging-operation 605-1
+    rate-limit upstream 39424 39424
+    rate-limit downstream 39424 39424
+   uni eth 4
+    extended-vlan-tagging-operation 605-1
+    rate-limit upstream 39424 39424
+    rate-limit downstream 39424 39424
+  bridge 2
+   ani mapper 2
+   link ip-host-config 1
+  ip-host-config 1
+   ip address static
+   extended-vlan-tagging-operation 11
+  apply
+ !
+ onu-profile S605_0n4b_35 create
+  traffic-profile S605_0n4b_35
+  apply
+ !
+";
+
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
