ec870495ca79479fd1a6346a9966ded04d9558d6
tom30913 tom30913 <tom90440@gmail.com>
2019-07-09 18:14:08 +0800

    Ver2

--- AutoOpen/Form1.Designer.cs
+++ AutoOpen/Form1.Designer.cs
@@ -33,7 +33,12 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -66,7 +71,7 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(104, 100);
+            this.richTextBox1.Location = new System.Drawing.Point(104, 136);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(413, 224);
             this.richTextBox1.TabIndex = 3;
@@ -76,7 +81,7 @@
             // 
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Items.AddRange(new object[] {
-            "T100",
+            "T100 (中正區)",
             "T103 (大同區)",
             "T104 (中山區)",
             "T105 (松山區)",
@@ -95,24 +100,74 @@
             this.comboBox2.Text = "行政區";
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
             // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(266, 13);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 5;
+            // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(279, 10);
+            this.textBox3.Location = new System.Drawing.Point(104, 76);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 5;
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(235, 76);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(359, 76);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "確認";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(694, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "導入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.Name = "Form1";
             this.Text = " ";
             this.ResumeLayout(false);
@@ -127,7 +182,12 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- AutoOpen/Form1.cs
+++ AutoOpen/Form1.cs
@@ -12,12 +12,22 @@ namespace AutoOpen
 {
     public partial class Form1 : Form
     {
-        
- 
-        public Form1()
-        {
-            InitializeComponent();
-
+        string[] T100 = { "011", "021", "031","041","061","071" };
+        string[] T103 = { "001", "011", "021", "031", "041", "051" };
+        string[] T104 = { "001", "011", "031", "041", "051", "061","071","081" };
+        string[] T105 = { "001","011", "021", "031", "041", "051","061", "071","081" };
+        string[] T106 = { "001", "011", "021", "031", "041", "051", "061", "071", "081","101","121" };
+        string[] T108 = { "011", "021", "031", "041", "051", "061" };
+        string[] T110 = { "001", "021", "031", "041", "051","061", "081","091","101" };
+        string[] T111 = { "001","011", "021", "031", "041", "061", "071","081", "091", "101","111" };
+        string[] T112 = { "001", "011", "031", "041","051", "061", "071", "081" };
+        string[] T114 = { "001", "002", "004", "011", "021", "031", "041", "051", "061", "071", "081" ,"091","101"};
+        string[] T115 = { "011", "021", "031", "041", "051", "061" };
+        string[] T116 = { "011", "021", "031", "041", "051", "061", "071", "081", "091", "101" };
+        public Form1()                          
+        {                                       
+            InitializeComponent();              
+                                                
             //class1.CreateDictionary();
 
             
@@ -30,68 +40,7 @@ namespace AutoOpen
             switch (ww)
             {
                 case "圖書館":
-                    richTextBox1.Text = $@" dba-profile m35_10% create
-mode sr
-sla maximum 39424
-apply
-!
- extended-vlan-tagging-operation 605-1 create
-  downstream-mode enable
-  untagged-frame 1
-   treat outer vid 605 cos 0 tpid 0x8100
-   treat inner vid 1 cos 0 tpid 0x8100
-  apply
- !
- traffic-profile S605_0n4b_35 create
-  mgmt-mode uni eth 1 omci
-  mgmt-mode uni eth 2 omci
-  mgmt-mode uni eth 3 omci
-  mgmt-mode uni eth 4 omci
-  tcont 1
-   gemport 1/1
-   dba-profile m35_10%
-  tcont 2
-   gemport 2/1
-   dba-profile 512k
-  mapper 1
-   gemport count 1
-   gemport 1 rate-limit upstream 39424 39424
-   gemport 1 rate-limit downstream 39424 39424
-  mapper 2
-   gemport count 1
-   gemport 1 rate-limit upstream 512 512
-   gemport 1 rate-limit downstream 512 512
-  bridge 1
-   ani mapper 1
-   uni eth 1
-    extended-vlan-tagging-operation 605-1
-    rate-limit upstream 39424 39424
-    rate-limit downstream 39424 39424
-   uni eth 2
-    extended-vlan-tagging-operation 605-1
-    rate-limit upstream 39424 39424
-    rate-limit downstream 39424 39424
-   uni eth 3
-    extended-vlan-tagging-operation 605-1
-    rate-limit upstream 39424 39424
-    rate-limit downstream 39424 39424
-   uni eth 4
-    extended-vlan-tagging-operation 605-1
-    rate-limit upstream 39424 39424
-    rate-limit downstream 39424 39424
-  bridge 2
-   ani mapper 2
-   link ip-host-config 1
-  ip-host-config 1
-   ip address static
-   extended-vlan-tagging-operation 11
-  apply
- !
- onu-profile S605_0n4b_35 create
-  traffic-profile S605_0n4b_35
-  apply
- !
-";
+                    richTextBox1.Text = $@"";
 
                     break;
 
@@ -103,12 +52,113 @@ apply
 
         private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string te = comboBox2.SelectedItem.ToString()+"-"+textBox3.Text;
+            switch (comboBox2.SelectedItem.ToString())
+            {
+                case @"T100 (中正區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T100)
+                    {
+
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T103 (大同區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T103)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T104 (中山區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T104)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T105 (松山區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T105)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T106 (大安區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T106)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T108 (萬華區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T108)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T110 (信義區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T110)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T111 (士林區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T111)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T112 (北投區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T112)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T114 (內湖區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T114)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T115 (南港區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T115)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                case @"T116 (文山區)":
+                    comboBox3.Items.Clear();
+                    foreach (var item in T116)
+                    {
+                        comboBox3.Items.Add(item);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+          
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string te = comboBox2.SelectedItem.ToString() + "-" + comboBox3.SelectedItem.ToString();
             Console.WriteLine(te);
             Class1 class1 = new Class1();
             class1.CreateDictionary();
-            class1.read("T100-011");
-            
+            Console.WriteLine(class1.read(te) ); 
         }
     }
 }
